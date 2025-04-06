4ef52ce939adb386498c5abe5a06b6eec13cf2b2
daann <daan.anne@ugent.com>
2025-04-06 22:21:25 +0200

    uitleg beter

--- app.py
+++ app.py
@@ -292,7 +292,7 @@ def serve_layout():
             # Box met etappelijnbeheer en grafiek
             html.Div([
                 html.H4("Etappelijn beheer", style={"marginBottom": "5px"}),
-                html.P("Voeg toe en verwijder met onderstaande knoppen. Je kan de lijnen verschuiven door erop te klikken en te slepen. De tabel onder de grafiek wordt zo automatsich geüpdatet.",  # noqa: E501
+                html.P("Voeg toe en verwijder met onderstaande knoppen. Je kan de lijnen verschuiven door erop te klikken en te slepen. De tabel onder de grafiek wordt zo automatisch geüpdatet.",  # noqa: E501
                        style={"marginBottom": "15px", "fontStyle": "italic"}),
                 html.Div([
                     html.Button(
