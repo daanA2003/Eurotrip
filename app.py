731e80a0264c372c65e581c1068a560ec9ac9e1f
daann <daan.anne@ugent.com>
2025-04-06 21:39:33 +0200

    path name

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ def parse_gpx(gpx_path):
     return afstanden, hoogtes, latitudes, longitudes
 
 # === Data voorbereiden ===
-gpx_path = "parcours.gpx"
+gpx_path = gpx_path = os.path.join(os.path.dirname(__file__),"parcours.gpx")
 x_data, y_data, lat_data, lon_data = parse_gpx(gpx_path)
 
 
